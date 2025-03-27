f1ceed5a2b127b3155a02a2d378121d64a2d9862
AndresOrtegaGuerrero <oandresg15@gmail.com>
2025-03-27 10:55:45 +0000

    Create pythonhob code

--- src/aiidalab_qe_pp/__main__.py
+++ src/aiidalab_qe_pp/__main__.py
@@ -4,6 +4,7 @@ from aiida.orm import load_code
 from aiida import load_profile
 import click
 from pathlib import Path
+import shutil
 import os
 
 """
@@ -29,6 +30,37 @@ def cli():
 @cli.command(help="Setup all executables and aiida codes in one go.")
 def post_install():
     click.echo("Starting full installation...")
+
+    click.echo("Setting up python3@localhost...")
+    load_profile()
+    try:
+        load_code("python3@localhost")
+    except NotExistent:
+        # Use shutil.which to find the path of the phonopy executable
+        python_path = shutil.which("python3")
+        if not python_path:
+            raise FileNotFoundError("python3 code is not found in PATH")
+        # Construct the command as a list of arguments
+        command = [
+            "verdi",
+            "code",
+            "create",
+            "core.code.installed",
+            "--non-interactive",
+            "--label",
+            "python3",
+            "--default-calc-job-plugin",
+            "pythonjob.pythonjob",
+            "--computer",
+            "localhost",
+            "--filepath-executable",
+            python_path,
+        ]
+
+        # Use subprocess.run to run the command
+        subprocess.run(command, check=True)
+    else:
+        print("Code python3@localhost is already installed! Nothing to do here.")
 
     click.echo("Setting up gfortran...")
     try:
